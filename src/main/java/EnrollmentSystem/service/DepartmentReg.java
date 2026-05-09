762dacacdd5bbd1fad7cb87cd1b4357347479fc3
John Matthew I. Malabag <jm.malabag05@gmail.com>
2026-05-09 22:51:09 +0800

    Adjusted Exception File

--- src/main/java/EnrollmentSystem/service/DepartmentReg.java
+++ src/main/java/EnrollmentSystem/service/DepartmentReg.java
@@ -6,7 +6,8 @@ import EnrollmentSystem.model.Section;
 import java.util.List;
 
 public interface DepartmentReg {
-    void addDepartment(String departmentID, String departmentName, List<Instructor> instructorList, List<Section> sectionList);
+    void addDepartment(String departmentID, String departmentName, List<Instructor> instructorList,
+                       List<Section> sectionList);
     void DisplayDepartments();
     void viewAllDepartmentsWithSections();
     void UpdateDepartment(Department department);
